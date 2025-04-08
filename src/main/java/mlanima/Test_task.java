0a02e5bf7ff73b26588b756d8a554bb20541cfca
Danylo Konovalenko <danylokonovalenko49@gmail.com>
2025-04-08 18:57:24 +0200

    add solution

---
+++ src/main/java/mlanima/Test_task.java
@@ -0,0 +1,125 @@
+package mlanima;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.Instant;
+import java.util.*;
+import java.util.function.Predicate;
+
+/**
+ * For implement this task focus on clear code, and make this solution as simple readable as possible
+ * Don't worry about performance, concurrency, etc
+ * You can use in Memory collection for sore data
+ * <p>
+ * Please, don't change class name, and signature for methods save, search, findById
+ * Implementations should be in a single class
+ * This class could be auto tested
+ */
+class DocumentManager {
+
+
+    private final Map<String, Document> documents = new HashMap<>();
+
+    /**
+     * Implementation of this method should upsert the document to your storage
+     * And generate unique id if it does not exist, don't change [created] field
+     *
+     * @param document - document content and author data
+     * @return saved document
+     */
+    public Document save(Document document) {
+        documents.put(document.getId(), document);
+        return document;
+    }
+
+    private Predicate<Document> searchFilter(SearchRequest request) {
+        List<Predicate<Document>> searchFilters = new ArrayList<>();
+
+        // filter by prefixes
+        if (request.getTitlePrefixes() != null) {
+            searchFilters.add(
+                    d -> request
+                            .getTitlePrefixes().stream()
+                            .anyMatch(prefix -> d.getTitle().startsWith(prefix))
+            );
+        }
+
+        // filter by contents
+        if ( request.getContainsContents() != null) {
+            searchFilters.add(
+                    d -> request.getContainsContents().stream()
+                            .anyMatch( content -> d.getContent().contains(content) )
+            );
+        }
+
+        // filter by authors' ids
+        if (request.getAuthorIds() != null) {
+            searchFilters.add(
+                    d -> request.getAuthorIds().contains(d.getId())
+            );
+        }
+
+        // filter by creationFrom
+        if (request.getCreatedFrom() != null) {
+            searchFilters.add(d -> d.getCreated().isAfter(request.getCreatedFrom()));
+        }
+
+        // filter by creationTo
+        if (request.getCreatedTo() != null) {
+            searchFilters.add(d -> d.getCreated().isBefore(request.getCreatedTo()));
+        }
+
+
+        return searchFilters.stream().reduce( f -> true, Predicate::and);
+    }
+
+    /**
+     * Implementation this method should find documents which match with request
+     *
+     * @param request - search request, each field could be null
+     * @return list matched documents
+     */
+
+    public List<Document> search(SearchRequest request) {
+        return documents.values().stream().filter(searchFilter(request)).toList();
+    }
+
+    /**
+     * Implementation this method should find document by id
+     *
+     * @param id - document id
+     * @return optional document
+     */
+    public Optional<Document> findById(String id) {
+        return Optional.ofNullable(documents.get(id));
+    }
+
+    @Data
+    @Builder
+    public static class SearchRequest {
+        private List<String> titlePrefixes;
+        private List<String> containsContents;
+        private List<String> authorIds;
+        private Instant createdFrom;
+        private Instant createdTo;
+    }
+
+    @Data
+    @Builder
+    public static class Document {
+        private String id;
+        private String title;
+        private String content;
+        private Author author;
+        private Instant created;
+    }
+
+    @Data
+    @Builder
+    public static class Author {
+        private String id;
+        private String name;
+    }
+
+}
---
+++ src/test/java/mlanima/DocumentManagerTest.java
@@ -0,0 +1,71 @@
+package mlanima;
+
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.Instant;
+import java.util.List;
+
+import static mlanima.DocumentManager.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DocumentManagerTest {
+
+    private DocumentManager manager;
+
+    @BeforeEach
+    void setUp() {
+        manager = new DocumentManager();
+
+        Author author = new Author("1", "Michel");
+        Document a = new Document("1", "My Document a", "Content a", author, Instant.MAX);
+        Document b = new Document("2", "My Document b", "Content b", author, Instant.MIN);
+        Document c = new Document("3", "My Document c", "Content c", author, Instant.now());
+
+        manager.save(a);
+        manager.save(b);
+        manager.save(c);
+
+    }
+
+    @Test
+    void save() {
+        Author author = new Author("1", "Michel");
+        Document b2 = new Document("1", "Changed document", "Content", author, Instant.now());
+
+        assertEquals(b2, manager.save(b2));
+    }
+
+    @Test
+    void searchByNullInstants() {
+        SearchRequest request = new SearchRequest(List.of("My"), List.of("a"), List.of("1", "2"), null, null);
+        assertFalse(manager.search(request).isEmpty());
+    }
+
+    @Test
+    void searchByNulls() {
+        SearchRequest request = new SearchRequest(null, null, null, null, null);
+        assertFalse(manager.search(request).isEmpty());
+    }
+
+    @Test
+    void searchBeforeInstant() {
+        SearchRequest request = new SearchRequest(null, null, null, null, Instant.now());
+        assertEquals(manager.search(request).size(), 2);
+        assertNotEquals(manager.search(request).getFirst().getId(), "1");
+    }
+
+    @Test
+    void searchAfterInstant() {
+        SearchRequest request = new SearchRequest(null, null, null, Instant.now(), null);
+        assertEquals(manager.search(request).size(), 1);
+    }
+
+    @Test
+    void findById() {
+        assertTrue(manager.findById("1").isPresent());
+        assertEquals(manager.findById("1").get().getId(), "1");
+    }
+}
